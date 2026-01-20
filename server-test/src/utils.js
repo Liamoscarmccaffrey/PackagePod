4e7ee61df9f6d6b5ab7937cfde7e307cc92dc029
Liamoscarmccaffrey <80630000+Liamoscarmccaffrey@users.noreply.github.com>
2026-01-20 10:24:05 +0000

    File clean up

--- server-test/src/utils.js
+++
@@ -1,7 +0,0 @@
-export async function copyFile(pod, path) {
-  const f = await pod.createFile("/"+path, "binary");
-  const resp = await fetch(path);
-  const buf = await resp.arrayBuffer();
-  await f.write(buf);
-  await f.close();
-}
